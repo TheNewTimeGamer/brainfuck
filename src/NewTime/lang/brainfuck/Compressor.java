7c4d352d0e86af3fd6f95fb98d9777a10ece40a6
TheNewTimeGamer <stefan.voorderhake@live.nl>
2019-08-20 14:42:06 +0200

    Support for uneven amount of operators.

--- src/NewTime/lang/brainfuck/Compressor.java
+++ src/NewTime/lang/brainfuck/Compressor.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Compressor {
 
 	public static void main(String[] args) {
-		byte[] operators = {4,5,6,7};
+		byte[] operators = {1,0,1,0,1,1,0};
 		System.out.println(Arrays.toString(operators));
 		Compressor compressor = new Compressor();
 		byte[] compressed = compressor.compress(operators);
@@ -15,33 +15,43 @@ public class Compressor {
 	}
 	
 	public byte[] compress(byte[] operators) {
-		byte[] buffer = new byte[operators.length/2];
+		byte[] buffer = null;
+		if(operators.length % 2 == 0) {
+			buffer = new byte[(operators.length/2)];
+		}else {
+			buffer = new byte[(operators.length/2)+1];
+		}
 		
 		for(int i = 0; i < operators.length; i+=2) {
 			byte start = operators[i];
+			if(i+1 >= operators.length) {
+				buffer[i/2] = start;
+				break;
+			}
 			byte end = operators[i+1];
-			System.out.print("Values: " + start + ", " + end);
 			end = (byte) (end << 4);
-			System.out.print(" -> " + start + ", " + end);
 			buffer[i/2] = (byte) (start | end);
-			System.out.println(" = " + start + " + " + end + " = " + ((byte)(start | end)));
 		}
 		return buffer;
 	}
 	
 	public byte[] decompress(byte[] operators) {
-		byte[] buffer = new byte[operators.length*2];
-		
+		byte[] buffer = null;
+		if(operators[operators.length-1] < 8) {
+			buffer = new byte[operators.length*2-1];
+		}else {
+			buffer = new byte[operators.length];
+		}		
 		for(int i = 0; i < operators.length; i++) {
 			byte start = (byte) ((operators[i] << (8*3+4)) >> (8*3+4));
 			byte end = (byte) (operators[i] >> 4);
 			
-			System.out.println(operators[i] + " = " + start + ", " + end + " -> " + i + " & " + (i+1));
-			
 			buffer[i*2] = start;
-			buffer[i*2+1] = end;
+			if(i*2+1 < buffer.length) {
+				buffer[i*2+1] = end;
+			}
 		}
-		
+
 		return buffer;
 	}
 	
